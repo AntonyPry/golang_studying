54b4b8e04bb29c0b6dc10f19666ad32a515e32c8
antonypry <pritov_as@resanta.ru>
2026-02-04 15:17:29 +0300

    select

---
+++ 11_select/racer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"time"
+)
+
+const tenSecondTimeout = 10 * time.Second
+
+var ErrTenSecondsLeft = errors.New("10 seconds left")
+
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", ErrTenSecondsLeft
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
