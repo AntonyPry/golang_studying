9ccafb3955d8d81666fded3db45b92ad5a9a0781
antonypry <pritov_as@resanta.ru>
2026-02-26 18:34:11 +0300

    math

--- 16_maths/clockface_acceptance_test.go
+++ 16_maths/clockface_acceptance_test.go
@@ -1,7 +1,6 @@
 package clockface_test
 
 import (
-	"strings"
 	"testing"
 	"time"
 
@@ -27,19 +26,5 @@ func TestSecondHandAt30Seconds(t *testing.T) {
 
 	if got != want {
 		t.Errorf("Got %v, wanted %v", got, want)
-	}
-}
-
-func TestSVGWriterAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	var b strings.Builder
-	clockface.SVGWriter(&b, tm)
-	got := b.String()
-
-	want := `<line x1="150" y1="150" x2="150" y2="60"`
-
-	if !strings.Contains(got, want) {
-		t.Errorf("Expected to find the second hand %v, in the SVG output %v", want, got)
 	}
 }
